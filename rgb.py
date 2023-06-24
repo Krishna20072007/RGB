7c074de86a7821a915274ab28f5d254e173bcbe2
Krishna K Lalwani <krishnakashab@gmail.com>
2023-06-24 17:32:10 +0530

    Initial Commit

--- rgb.py
+++ rgb.py
@@ -1,59 +1,46 @@
-print("Starting now")
-import time
-from openpyxl import Workbook
-from openpyxl.styles import PatternFill, Alignment, Font
+import openpyxl
+from openpyxl.styles import Font, Alignment
+import os
 
 end = 255
 
-start_time = time.time()
+# Create the Excels folder if it doesn't exist
+if not os.path.exists('Excels'):
+    os.makedirs('Excels')
 
-wb = Workbook()
-ws = wb.active
-ws.title = "RGB"
-
-for r in range(0, end + 1):
-    for g in range(0, end + 1):
-        for b in range(0, end + 1):
-            ws.append([r, g, b])
-        print(f"{r}, {g}")
-
-    ws['A1'] = "R"
-    ws['B1'] = "G"
-    ws['C1'] = "B"
-
-    # Set background color for each row
-    for row in ws.iter_rows(min_row=2, min_col=1, max_col=3):
-        r, g, b = [cell.value for cell in row]
-        color = f"{r:02X}{g:02X}{b:02X}"  # Convert RGB to hexadecimal color
-        fill = PatternFill(start_color=color, end_color=color, fill_type="solid")
-        for cell in row:
-            cell.fill = fill
-
-    # Create separate sheets for each R value
-    for r in range(0, end + 1):
-        new_ws = wb.create_sheet(title=str(r))
-        new_ws['A1'] = "R"
-        new_ws['B1'] = "G"
-        new_ws['C1'] = "B"
-
-        header_font = Font(bold=True)
-        header_alignment = Alignment(horizontal='center')
-
-        for col in new_ws.iter_cols(min_row=1, max_row=1, min_col=1, max_col=3):
-            for cell in col:
-                cell.font = header_font
-                cell.alignment = header_alignment
-
-        for row in ws.iter_rows(min_row=2, min_col=1, max_col=3):
-            if row[0].value == r:
-                new_ws.append([cell.value for cell in row])
-
-
-wb.remove(ws)  # Remove the original "RGB" sheet
-
-wb.save("Excels/rgb.xlsx")
-
-end_time = time.time()
-execution_time = end_time - start_time
-
-print(f"It takes {execution_time*1000}ms to complete {end} numbers.")
+for r in range(0, end+1):
+    # Create a new Excel file
+    wb = openpyxl.Workbook()
+    
+    # Remove the original sheet
+    default_sheet = wb.active
+    wb.remove(default_sheet)
+    
+    for g in range(0, end+1):
+        # Create a new sheet for each g value
+        sheet = wb.create_sheet(title=f'{g}')
+        
+        # Set the column headers
+        sheet['A1'].value = 'R'
+        sheet['B1'].value = 'G'
+        sheet['C1'].value = 'B'
+        
+        # Format the column headers
+        bold_font = Font(bold=True)
+        center_alignment = Alignment(horizontal='center')
+        for col in ['A', 'B', 'C']:
+            cell = sheet[f'{col}1']
+            cell.font = bold_font
+            cell.alignment = center_alignment
+        
+        for b in range(0, end+1):
+            # Populate the data in each row
+            row = (r, g, b)
+            sheet.append(row)
+        
+        print(f'Appended g = {g} to sheet {g}, book {r}')
+    
+    # Save the Excel file
+    file_name = f'Excels/RGB_{r}.xlsx'
+    wb.save(file_name)
+    print(f'Excel file "{file_name}" created successfully.')
