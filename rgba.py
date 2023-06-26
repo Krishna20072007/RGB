2179db4786033b54f31990d2142791a29f846f9d
Krishna K Lalwani <krishnakashab@gmail.com>
2023-06-26 18:28:29 +0530

    Initial Commit

--- rgba.py
+++ rgba.py
@@ -9,46 +9,44 @@ if not os.path.exists('RGBA_Excels'):
     os.makedirs('RGBA_Excels')
 
 for a in range(0, end + 1):
-    # Create a new Excel file for each alpha value
-    wb = openpyxl.Workbook()
-
-    # Remove the original sheet
-    default_sheet = wb.active
-    wb.remove(default_sheet)
+    # Create a new folder for each alpha value
+    folder_name = f'RGBA_Excels/{a}'
+    os.makedirs(folder_name, exist_ok=True)
 
     for r in range(0, end + 1):
-        # Create a new sheet for each red value
-        sheet = wb.create_sheet(title=f'{r}')
+        # Create a new Excel file for each red value
+        file_name = f'{folder_name}/RGBA_{r}.xlsx'
+        wb = openpyxl.Workbook()
 
-        # Set the column headers
-        sheet['A1'].value = 'R'
-        sheet['B1'].value = 'G'
-        sheet['C1'].value = 'B'
-        sheet['D1'].value = 'A'
-
-        # Format the column headers
-        bold_font = Font(bold=True)
-        center_alignment = Alignment(horizontal='center')
-        for col in ['A', 'B', 'C', 'D']:
-            cell = sheet[f'{col}1']
-            cell.font = bold_font
-            cell.alignment = center_alignment
+        # Remove the default sheet
+        default_sheet = wb.active
+        wb.remove(default_sheet)
 
         for g in range(0, end + 1):
+            # Create a new sheet for each green value
+            sheet = wb.create_sheet(title=f'{g}')
+
+            # Set the column headers
+            sheet['A1'].value = 'R'
+            sheet['B1'].value = 'G'
+            sheet['C1'].value = 'B'
+            sheet['D1'].value = 'A'
+
+            # Format the column headers
+            bold_font = Font(bold=True)
+            center_alignment = Alignment(horizontal='center')
+            for col in ['A', 'B', 'C', 'D']:
+                cell = sheet[f'{col}1']
+                cell.font = bold_font
+                cell.alignment = center_alignment
+
             for b in range(0, end + 1):
                 # Populate the data in each row
                 row = (r, g, b, a)
                 sheet.append(row)
 
-            print(f'{r},{g},{b},{a}')
+                print(f'{r},{g},{b},{a}')
 
-        # Remove the RGBA sheet from memory
-        wb.remove(sheet)
-
-    # Save the Excel file
-    file_name = f'RGBA_Excels/RGBA_{a}.xlsx'
-    wb.save(file_name)
-    print(f'Excel file "{file_name}" created successfully.')
-
-    # Print alpha (a) value
-    print(f'Alpha (a): {a}')
+        # Save the Excel file
+        wb.save(file_name)
+        print(f'Excel file "{file_name}" created successfully for alpha (a): {a}, red (r): {r}.')
